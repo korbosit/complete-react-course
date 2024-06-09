05cace11456d7281e3c2ef377396ccb2ba18b92b
Vlaimir Korba <krissky1986@gmail.com>
2024-06-09 12:35:54 +0300

    06_Creating_a_Component

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+function App() {
+    return <p>This is my first Component</p>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
+import App from "./app";
 
-const header = <h1>This is my first React App</h1>;
+// const header = <h1>This is my first React App</h1>;
 
 // console.log(header);
 
-ReactDom.render(header, document.getElementById("root"));
+// ReactDom.render(header, document.getElementById("root"));
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
