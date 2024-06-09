8c311a6a6735145eda7aec5e050eaf704dc4d87f
Vlaimir Korba <krissky1986@gmail.com>
2024-06-09 13:19:00 +0300

    08_Adding_CSS_to_Component

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,5 @@
+import "./app.css";
+
 function App() {
     // const p = document.createElement("p");
     // p.textContent = "This is a demo paragraph";
@@ -5,11 +7,10 @@ function App() {
     let msg = "awesome";
     return (
         <>
-            <h1>This is my header</h1>
-            <p>
+            <h1 className="myHeader">This is my header</h1>
+            <p className="myParagraph">
                 This is my first {msg} Component {10 + 5}
             </p>
-            <input type="text" />
         </>
     );
 }
