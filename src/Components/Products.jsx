88a3e3f212c2d1bed3c367187493f9973e0b99fc
Vlaimir Korba <krissky1986@gmail.com>
2024-06-18 20:25:05 +0300

    15_Passing_data_via_props

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,8 +1,7 @@
 let productCount = 0;
-let imageUrl = require("../images/ukhilyant.png");
+// let imageUrl = require("../images/fresh-milk.png");
 let isAvailable = "Unavailable";
-let badgeClass = "badge-margin-left-240 badge";
-badgeClass += isAvailable === "Available" ? " bg-success " : " bg-danger";
+
 // let style = {
 //     padding: "0px 20px",
 //     // fontSize: 14,
@@ -13,21 +12,27 @@ function displayFormattedProductCount() {
     return productCount > 0 ? productCount : "Zero";
 }
 
-function Products() {
+function Products(props) {
+    let badgeClass = "badge-margin-left-240 badge";
+    badgeClass += props.isAvailable ? " bg-success " : " bg-danger";
     return (
         <ul className="list-group shadow">
             <li className="list-group-item">
                 <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                     <div className="media-body order-2 order-lg-1">
                         <h5 className="mt-0 font-weight-bold mb-2">
-                            Fresh Milk
+                            {props.name}
                         </h5>
                         <p className="font-italic text-muted mb-0 small">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Suscipit fuga autem maiores necessitatibus.
+                            {props.description}
                         </p>
                         <div className="d-flex align-items-center justify-content-start mt-1">
-                            <h6 className="font-weight-bold my-2">$120.00</h6>
+                            <h6
+                                className="font-weight-bold my-2"
+                                style={{ "margin-right": 30 }}
+                            >
+                                ${props.price}
+                            </h6>
                             <button
                                 className="btn btn-primary"
                                 style={{ "margin-right": 30 }}
@@ -35,17 +40,21 @@ function Products() {
                                 -
                             </button>
                             <span
-                                style={{ padding: "0px 14px", "font-size": 13 }}
+                                style={{ padding: "8px 14px", "font-size": 13 }}
                             >
                                 {displayFormattedProductCount()}
                             </span>
                             <button className="btn btn-primary">+</button>
-                            <span className={badgeClass}>{isAvailable}</span>
+                            <span className={badgeClass}>
+                                {props.isAvailable
+                                    ? "Available"
+                                    : "Unavailable"}
+                            </span>
                         </div>
                     </div>
                     <img
-                        src={imageUrl}
-                        alt="Ykhylyant"
+                        src={props.imageUrl}
+                        alt="Fresh Milk"
                         width="200"
                         className="ml-lg-5 order-1 order-lg-2"
                     />
