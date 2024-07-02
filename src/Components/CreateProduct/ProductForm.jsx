23303f1e4002c4c411e14d98c8410185ead3b210
Vlaimir Korba <krissky1986@gmail.com>
2024-07-02 14:10:13 +0300

    26 Using a Single state

--- src/Components/CreateProduct/ProductForm.jsx
+++ src/Components/CreateProduct/ProductForm.jsx
@@ -1,26 +1,54 @@
 import React, { useState } from "react";
 
 function ProductForm() {
-    let [pName, updateName] = useState("");
-    let [pPrice, updatePrice] = useState("");
-    let [pDescription, updateDescription] = useState("");
-    let [pAvailability, updateAvailability] = useState("");
-    let [pImage, updateUImageUrl] = useState("");
+    // let [pName, updateName] = useState("");
+    // let [pPrice, updatePrice] = useState("");
+    // let [pDescription, updateDescription] = useState("");
+    // let [pAvailability, updateAvailability] = useState("");
+    // let [pImage, updateUImageUrl] = useState("");
+
+    let [userInput, updateUserInput] = useState({
+        pName: "",
+        pPrice: "",
+        pDescription: "",
+        pAvailability: "",
+        pImage: "",
+    });
 
     function nameInputHandler(event) {
-        updateName(event.target.value);
+        // updateName(event.target.value);
+        updateUserInput({
+            ...userInput,
+            pName: event.target.value,
+        });
     }
     function priceInputHandler(event) {
-        updatePrice(event.target.value);
+        // updatePrice(event.target.value);
+        updateUserInput({
+            ...userInput,
+            pPrice: event.target.value,
+        });
     }
     function descriptionInputHandler(event) {
-        updateDescription(event.target.value);
+        // updateDescription(event.target.value);
+        updateUserInput({
+            ...userInput,
+            pDescription: event.target.value,
+        });
     }
     function availabilityInputHandler(event) {
-        updateAvailability(event.target.value);
+        // updateAvailability(event.target.value);
+        updateUserInput({
+            ...userInput,
+            pAvailability: event.target.value,
+        });
     }
     function imageInputHandler(event) {
-        updateUImageUrl(event.target.value);
+        // updateUImageUrl(event.target.value);
+        updateUserInput({
+            ...userInput,
+            pImage: event.target.value,
+        });
     }
     return (
         <form className="row g-3">
