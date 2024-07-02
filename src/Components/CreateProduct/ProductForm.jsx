497d30d1c10d9a966b3b94379b49e5f67f891573
Vlaimir Korba <krissky1986@gmail.com>
2024-07-02 13:41:18 +0300

    26_Using_a_Single_state

--- src/Components/CreateProduct/ProductForm.jsx
+++ src/Components/CreateProduct/ProductForm.jsx
@@ -1,18 +1,26 @@
+import React, { useState } from "react";
+
 function ProductForm() {
+    let [pName, updateName] = useState("");
+    let [pPrice, updatePrice] = useState("");
+    let [pDescription, updateDescription] = useState("");
+    let [pAvailability, updateAvailability] = useState("");
+    let [pImage, updateUImageUrl] = useState("");
+
     function nameInputHandler(event) {
-        console.log(event.target.value);
+        updateName(event.target.value);
     }
     function priceInputHandler(event) {
-        console.log(event.target.value);
+        updatePrice(event.target.value);
     }
     function descriptionInputHandler(event) {
-        console.log(event.target.value);
+        updateDescription(event.target.value);
     }
     function availabilityInputHandler(event) {
-        console.log(event.target.value);
+        updateAvailability(event.target.value);
     }
     function imageInputHandler(event) {
-        console.log(event.target.value);
+        updateUImageUrl(event.target.value);
     }
     return (
         <form className="row g-3">
