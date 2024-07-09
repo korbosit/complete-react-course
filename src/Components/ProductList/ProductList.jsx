dc4499090a1efa141279d045e12c42c790abdfdf
Vlaimir Korba <krissky1986@gmail.com>
2024-07-09 19:29:21 +0300

    #32_Using_Stateful_List_in_React

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,63 +1,23 @@
+import { useState } from "react";
 import Products from "./Products";
 
-let products = [
-    {
-        pID: 1,
-        pName: "Fresh Milk",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.",
-        isAvailable: true,
-        image: "images/fresh-milk.png",
-        price: 12,
-    },
-    {
-        pID: 2,
-        pName: "Cottage Cheese",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.",
-        isAvailable: false,
-        image: "images/cottage-cheese.png",
-        price: 10,
-    },
-    {
-        pID: 3,
-        pName: "Brocoli",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.",
-        isAvailable: true,
-        image: "images/brocoli.png",
-        price: 15,
-    },
-    {
-        pID: 4,
-        pName: "oranges",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.",
-        isAvailable: true,
-        image: "images/oranges.png",
-        price: 20,
-    },
-    {
-        pID: 5,
-        pName: "Olive oil",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.",
-        isAvailable: false,
-        image: "images/olive-oil.png",
-        price: 14,
-    },
-];
-
 function ProductList(props) {
-    console.log(props.newProduct);
+    // console.log(props.newProduct);
+    // let [newProductList, updateProductList] = useState(products);
+    // updateProductList([props.newProduct, ...products]);
     return (
         <div className="row">
             <div className="col-lg-8 mx-auto">
                 <ul className="list-group shadow">
-                    {products.map((product) => {
+                    {props.newProductList.map((product) => {
                         return (
                             <Products
-                                id={product.pID}
-                                name={product.pName}
-                                description={product.desc}
-                                isAvailable={product.isAvailable}
-                                imageUrl={product.image}
-                                price={product.price}
+                                id={product?.pID}
+                                name={product?.pName}
+                                description={product?.desc}
+                                isAvailable={product?.isAvailable}
+                                imageUrl={product?.image}
+                                price={product?.price}
                             />
                         );
                     })}
